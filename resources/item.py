012d12b4ca93da0868dcc8547be472257e9b13d5
Magnus Henkel <loxosceles@gmx.de>
2020-09-23 19:33:37 +0200

    Rollback

--- resources/item.py
+++ resources/item.py
@@ -1,19 +1,22 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
+import sqlite3
 from models.item import ItemModel
+
 
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
-    )
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
     parser.add_argument('store_id',
-        type=int,
-        required=True,
-        help="Every item needs a store id."
-    )
+                        type=int,
+                        required=True,
+                        help="Every item needs a store id!"
+                        )
 
     @jwt_required()
     def get(self, name):
@@ -24,7 +27,7 @@ class Item(Resource):
 
     def post(self, name):
         if ItemModel.find_by_name(name):
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+            return {'message': "An item with name '{}' already exists.".format(name)}
 
         data = Item.parser.parse_args()
 
@@ -33,20 +36,13 @@ class Item(Resource):
         try:
             item.save_to_db()
         except:
-            return {"message": "An error occurred inserting the item."}, 500
+            return {"message": "An error occurred inserting the item."}
 
         return item.json(), 201
 
-    def delete(self, name):
-        item = ItemModel.find_by_name(name)
-        if item:
-            item.delete_from_db()
-
-        return {'message': 'Item deleted'}
-
+    @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
-
         item = ItemModel.find_by_name(name)
 
         if item is None:
@@ -58,7 +54,14 @@ class Item(Resource):
 
         return item.json()
 
+    @jwt_required()
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+
+        return {"message": "Item deleted"}
 
 class ItemList(Resource):
     def get(self):
-        return {'items': [x.json() for x in ItemModel.query.all()]}
+        return {'items': [item.json() for item in ItemModel.query.all()]}
